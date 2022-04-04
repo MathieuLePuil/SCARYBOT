74089e591b9c72d4700aa9b05552af77ffc7a9c6
MathieuLePuil <mathieu.le-puil@etudiant.univ-reims.fr>
2022-04-04 11:50:51 +0200

    Auto-Commit

--- giveaway.py
+++ giveaway.py
@@ -80,7 +80,7 @@ class Giveaway(commands.Cog):
         if interactions.custom_id == "giveaway":
             await interactions.respond(type=7)
 
-            em1 = discord.Embed(description="Dans quel channel souhaitez-vous lancer le giveaway?",
+            em1 = discord.Embed(description="Quel est le lot (simplifié) du Giveaway ? (nom du salon)",
                                 color=0xFFA500)
             em2 = discord.Embed(description="Quelle est la durée du Giveaway? (s | m | d | h)", color=0xFFA500)
             em3 = discord.Embed(description="Quel est le lot du Giveaway?",
@@ -99,15 +99,6 @@ class Giveaway(commands.Cog):
                 await interactions.channel.purge(limit=1, check=lambda msg: not msg.pinned)
                 await interactions.channel.send("Vous avez été trop long, veuillez recommencer.", delete_after=10)
                 return
-
-            digits = "0123456789"
-            channelg_id = ""
-
-            for i in channelg.content:
-                if i in digits:
-                    channelg_id += i
-
-            channelg_id = int(channelg_id)
 
             await interactions.channel.send(embed=em2)
 
@@ -202,7 +193,10 @@ class Giveaway(commands.Cog):
             else:
                 minute = fin.minute
 
-            gchannel = self.bot.get_channel(channelg_id)
+            catego = self.bot.get_channel(821466113132724244)
+
+            gchannel = await guild.create_text_channel(f"🎉〡giveaway-{channelg}", category=catego)
+            channelg_id = gchannel.id
             guild.get_role(rolecond_id)
 
             embed = discord.Embed(title=f"**{lot.content}**",
